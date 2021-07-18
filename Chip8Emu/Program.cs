4ac4379826ec434e0bd4aeb9b4623533548c5926
ironlanderl <fabrizio03p@gmail.com>
2021-07-18 23:44:38 +0200

    SDL draws random pixels!

--- Chip8Emu/Program.cs
+++ Chip8Emu/Program.cs
@@ -9,11 +9,7 @@ namespace Chip8Emu
 {
     static class Program
     {
-        public static bool refr = false;
-        /// <summary>
-        /// Punto di ingresso principale dell'applicazione.
-        /// </summary>
-        [STAThread]
+        static Random rng = new Random();
         static void Main()
         {
             // Initilizes SDL.
@@ -26,8 +22,8 @@ namespace Chip8Emu
             var window = SDL.SDL_CreateWindow("SDL .NET 5 Tutorial",
                                               SDL.SDL_WINDOWPOS_UNDEFINED,
                                               SDL.SDL_WINDOWPOS_UNDEFINED,
-                                              640,
-                                              480,
+                                              800,
+                                              600,
                                               SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
 
             if (window == IntPtr.Zero)
@@ -54,28 +50,42 @@ namespace Chip8Emu
 
             var running = true;
 
+            // Sets the color that the screen will be cleared with.
+            if (SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) < 0)
+            {
+                Console.WriteLine($"There was an issue with setting the render draw color. {SDL.SDL_GetError()}");
+            }
+
+            // Clears the current render surface.
+            if (SDL.SDL_RenderClear(renderer) < 0)
+            {
+                Console.WriteLine($"There was an issue with clearing the render surface. {SDL.SDL_GetError()}");
+            }
+
+            SDL.SDL_RenderSetIntegerScale(renderer, SDL.SDL_bool.SDL_TRUE);
+            SDL.SDL_RenderSetScale(renderer, 10, 10);
+
             // Main loop for the program
             while (running)
             {
-                // Check to see if there are any events and continue to do so until the queue is empty.
                 while (SDL.SDL_PollEvent(out SDL.SDL_Event e) == 1)
+                // Check to see if there are any events and continue to do so until the queue is empty.
                 {
                     switch (e.type)
                     {
                         case SDL.SDL_EventType.SDL_QUIT:
                             running = false;
                             break;
+                        case SDL.SDL_EventType.SDL_KEYDOWN:
+                            SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                            SDL.SDL_RenderClear(renderer);
+                            break;
                     }
                 }
 
-                // Sets the color that the screen will be cleared with.
-                if (SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255) < 0)
-                {
-                    Console.WriteLine($"There was an issue with setting the render draw color. {SDL.SDL_GetError()}");
-                }
-
-                // Clears the current render surface.
-                if (SDL.SDL_RenderClear(renderer) < 0)
+                // Change color to black
+                SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                if (SDL.SDL_RenderDrawPoint(renderer, rng.Next(0, 81), rng.Next(0, 61)) < 0)
                 {
                     Console.WriteLine($"There was an issue with clearing the render surface. {SDL.SDL_GetError()}");
                 }

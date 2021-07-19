77870d6074adf6bd2e188d03d29e45ccd005f6a7
ironlanderl <fabrizio03p@gmail.com>
2021-07-19 23:19:20 +0200

    Fixed drawing and instruction fetching.

--- Chip8Emu/Program.cs
+++ Chip8Emu/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,6 +11,22 @@ namespace Chip8Emu
     static class Program
     {
         static Random rng = new Random();
+        static Cpu cpu = new Cpu();
+
+        // Create a new window given a title, size, and passes it a flag indicating it should be shown.
+        static IntPtr window = SDL.SDL_CreateWindow("Chip8Emu.net",
+                                              SDL.SDL_WINDOWPOS_UNDEFINED,
+                                              SDL.SDL_WINDOWPOS_UNDEFINED,
+                                              640,
+                                              320,
+                                              SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+        // Creates a new SDL hardware renderer using the default graphics device with VSYNC enabled.
+        static IntPtr renderer = SDL.SDL_CreateRenderer(window,
+                                              -1,
+                                              SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED/* |
+                                              SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC*/);
+
         static void Main()
         {
             // Initilizes SDL.
@@ -18,29 +35,6 @@ namespace Chip8Emu
                 Console.WriteLine($"There was an issue initilizing SDL. {SDL.SDL_GetError()}");
             }
 
-            // Create a new window given a title, size, and passes it a flag indicating it should be shown.
-            var window = SDL.SDL_CreateWindow("SDL .NET 5 Tutorial",
-                                              SDL.SDL_WINDOWPOS_UNDEFINED,
-                                              SDL.SDL_WINDOWPOS_UNDEFINED,
-                                              800,
-                                              600,
-                                              SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
-
-            if (window == IntPtr.Zero)
-            {
-                Console.WriteLine($"There was an issue creating the window. {SDL.SDL_GetError()}");
-            }
-
-            // Creates a new SDL hardware renderer using the default graphics device with VSYNC enabled.
-            var renderer = SDL.SDL_CreateRenderer(window,
-                                                  -1,
-                                                  SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED |
-                                                  SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
-
-            if (renderer == IntPtr.Zero)
-            {
-                Console.WriteLine($"There was an issue creating the renderer. {SDL.SDL_GetError()}");
-            }
 
             // Initilizes SDL_image for use with png files.
             if (SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG) == 0)
@@ -48,19 +42,12 @@ namespace Chip8Emu
                 Console.WriteLine($"There was an issue initilizing SDL2_Image {SDL_image.IMG_GetError()}");
             }
 
+
+            Menu();
+
+
+
             var running = true;
-
-            // Sets the color that the screen will be cleared with.
-            if (SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) < 0)
-            {
-                Console.WriteLine($"There was an issue with setting the render draw color. {SDL.SDL_GetError()}");
-            }
-
-            // Clears the current render surface.
-            if (SDL.SDL_RenderClear(renderer) < 0)
-            {
-                Console.WriteLine($"There was an issue with clearing the render surface. {SDL.SDL_GetError()}");
-            }
 
             SDL.SDL_RenderSetIntegerScale(renderer, SDL.SDL_bool.SDL_TRUE);
             SDL.SDL_RenderSetScale(renderer, 10, 10);
@@ -83,11 +70,22 @@ namespace Chip8Emu
                     }
                 }
 
-                // Change color to black
+                // reset screen
+                SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL.SDL_RenderClear(renderer);
+
+                // Change color to white
                 SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                if (SDL.SDL_RenderDrawPoint(renderer, rng.Next(0, 81), rng.Next(0, 61)) < 0)
+                // Get the screen
+                bool[,] screen = cpu.GetScreen();
+                //Draw!
+                for (int i = 0; i < 32; i++)
                 {
-                    Console.WriteLine($"There was an issue with clearing the render surface. {SDL.SDL_GetError()}");
+                    for (int j = 0; j < 64; j++)
+                    {
+                        if (screen[i, j])
+                            SDL.SDL_RenderDrawPoint(renderer, j, i);
+                    }
                 }
 
                 // Switches out the currently presented render surface with the one we just did work on.
@@ -98,6 +96,14 @@ namespace Chip8Emu
             SDL.SDL_DestroyRenderer(renderer);
             SDL.SDL_DestroyWindow(window);
             SDL.SDL_Quit();
+        }
+
+        static void Menu()
+        {
+            // Just load the rom
+            byte[] ROM;
+            ROM = File.ReadAllBytes(@"C:\path\ibm.ch8");
+            cpu.LoadROM(ROM);
         }
     }
 }
